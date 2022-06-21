1b664a015e0cd9321a32571153468952dd3e25de
Michał Rubel <contact@observer.name>
2022-06-21 21:00:04 +0200

    optimize imports

--- src/Traits/BindsToContainer.php
+++ src/Traits/BindsToContainer.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace MichaelRubel\AutoBinder\Traits;
 
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\LazyCollection;
 use Illuminate\Support\Str;
