ba8933e652ad08ad28987fb8233efa3e02a3f618
Michał Rubel <contact@observer.name>
2022-07-10 12:58:16 +0200

    add clear command

---
+++ src/Commands/AutoBinderClearCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MichaelRubel\AutoBinder\Commands;
+
+use Illuminate\Cache\Repository;
+use Illuminate\Console\Command;
+use MichaelRubel\AutoBinder\AutoBinder;
+
+class AutoBinderClearCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'binder:clear {folder : Folder to clear the cache from}';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Clear a cached bindings';
+
+    /**
+     * @param  Repository  $cache
+     *
+     * @return void
+     */
+    public function handle(Repository $cache): void
+    {
+        $folder = $this->argument('folder');
+        $clue   = (new AutoBinder($folder))->cacheClue();
+
+        collect($cache->get($clue))->each(
+            fn ($concrete, $interface) => app()->offsetUnset($interface)
+        );
+
+        $cache->forget($clue);
+    }
+}
