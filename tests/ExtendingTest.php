f34512fa504030ad65f4059c9e73226aa6dee1e0
Michael Rubél <contact@observer.name>
2022-11-17 14:22:22 +0100

    `pint` fix 🍺

--- tests/ExtendingTest.php
+++ tests/ExtendingTest.php
@@ -2,17 +2,7 @@
 
 namespace MichaelRubel\AutoBinder\Tests;
 
-use MichaelRubel\AutoBinder\AutoBinder;
-use MichaelRubel\AutoBinder\Commands\AutoBinderClearCommand;
 use MichaelRubel\AutoBinder\Tests\Boilerplate\ExtendedAutoBinder;
-use MichaelRubel\AutoBinder\Tests\Boilerplate\Models\Example;
-use MichaelRubel\AutoBinder\Tests\Boilerplate\Models\Interfaces\ExampleInterface;
-use MichaelRubel\AutoBinder\Tests\Boilerplate\Services\AnotherService;
-use MichaelRubel\AutoBinder\Tests\Boilerplate\Services\ExampleService;
-use MichaelRubel\AutoBinder\Tests\Boilerplate\Services\Interfaces\AnotherServiceInterface;
-use MichaelRubel\AutoBinder\Tests\Boilerplate\Services\Interfaces\ExampleServiceInterface;
-use MichaelRubel\AutoBinder\Tests\Boilerplate\Services\Interfaces\TestServiceInterface;
-use MichaelRubel\AutoBinder\Tests\Boilerplate\Services\Test\TestService;
 
 class ExtendingTest extends TestCase
 {
