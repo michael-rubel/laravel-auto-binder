fa847760f806c1685a1d7b5e8d84c4a03e83c9f6
Michael Rubél <contact@observer.name>
2023-01-29 11:44:32 +0100

    Improve the cache clear command 🔧

--- src/Commands/AutoBinderClearCommand.php
+++ src/Commands/AutoBinderClearCommand.php
@@ -4,6 +4,7 @@ namespace MichaelRubel\AutoBinder\Commands;
 
 use Illuminate\Cache\Repository;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use MichaelRubel\AutoBinder\AutoBinder;
 
 class AutoBinderClearCommand extends Command
@@ -11,7 +12,7 @@ class AutoBinderClearCommand extends Command
     /**
      * @var string
      */
-    protected $signature = 'binder:clear {folder : Folder to clear the cache from}';
+    protected $signature = 'binder:clear {folders : Folders to clear the cache from}';
 
     /**
      * @var string
@@ -21,20 +22,69 @@ class AutoBinderClearCommand extends Command
     /**
      * @param  Repository  $cache
      *
-     * @return void
+     * @return int
      */
-    public function handle(Repository $cache): void
+    public function handle(Repository $cache): int
     {
-        $clue = (new AutoBinder(
-            $this->argument('folder')
-        ))->cacheClue();
+        $this->getFolders()->each(function ($folder) use ($cache) {
+            $clue = $this->getClueFor($folder);
 
-        collect($cache->get($clue))->each(
-            fn ($concrete, $interface) => app()->offsetUnset($interface)
+            if (! $cache->has($clue)) {
+                $this->components->info(
+                    'Cached folder ' . $folder . ' not found.'
+                );
+
+                return;
+            }
+
+            $this->flushContainerBindings($cache, $clue);
+
+            $cache->forget($clue);
+        });
+
+        $this->components->info(
+            'Container binding cache cleared successfully!'
         );
 
-        $cache->forget($clue);
+        return Command::SUCCESS;
+    }
 
-        $this->info('Container bindings and cache were cleared successfully!');
+    /**
+     * Retrieves folders to flush from the command input.
+     *
+     * @return Collection
+     */
+    private function getFolders(): Collection
+    {
+        $folders = explode(',', $this->argument('folders'));
+
+        return collect($folders);
+    }
+
+    /**
+     * Retrieves the cache clue for the specified folder.
+     *
+     * @param  string  $folder
+     *
+     * @return string
+     */
+    private function getClueFor(string $folder): string
+    {
+        return (new AutoBinder($folder))->cacheClue();
+    }
+
+    /**
+     * Cleans up the application container bindings.
+     *
+     * @param  Repository  $cache
+     * @param  string  $clue
+     *
+     * @return void
+     */
+    private function flushContainerBindings(Repository $cache, string $clue): void
+    {
+        with($cache->get($clue), fn ($fromCache) => collect($fromCache)->each(
+            fn ($concrete, $interface) => app()->offsetUnset($interface)
+        ));
     }
 }
